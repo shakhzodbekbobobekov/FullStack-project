c4b4091134f2357eda454bc39cca8104e646edd5
Acer <shaxzodbekbobobekov@gmail.com>
2022-12-19 01:42:43 +0500

    added react-router-dom and created components

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      Login Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa, odio
+      aspernatur fugiat repudiandae, neque praesentium, maxime laboriosam vero
+      molestias consectetur quos cum culpa. Harum necessitatibus, facere est
+      nulla repudiandae quidem!
+    </div>
+  );
+}
+
+export default Login;
